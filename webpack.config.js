55817c56ee5cda684f813208ccee0b2fb687a36f
Jonathan Inkemann <jonathan.inkemann@gmail.com>
2021-04-05 20:44:34 +0200

    Add babel to webpack chain

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,16 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "src", "index.html")
-    })
-  ]
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: ["babel-loader"]
+        }]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "src", "index.html")
+        })
+    ]
 };
