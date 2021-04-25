1733fdef9137536df3904b7179c96a78b740a38b
Jonathan Inkemann <jonathan.inkemann@gmail.com>
2021-04-25 16:02:52 +0200

    Update build settings.

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    id("dev.fritz2.fritz2-gradle") version "0.9.2"
+}
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    jvm()
+    js(IR) {
+        browser()
+    }.binaries.executable()
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation("dev.fritz2:core:0.9.2")
+                implementation("dev.fritz2:components:0.9.2")
+            }
+        }
+        val jvmMain by getting {
+            dependencies {
+            }
+        }
+        val jsMain by getting {
+            dependencies {
+            }
+        }
+    }
+}
